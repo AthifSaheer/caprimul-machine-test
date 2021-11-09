75ba3c7e7232197e28c531e47b0c5fc096c77dcd
Athif Saheer <liteboook@gmail.com>
2021-11-09 13:48:29 +0530

    todo create, edite and delete features added

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('edit/<int:id>/', views.edit, name="edit"),
+    path('delete/<int:id>/', views.delete, name="delete"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 def home(request):
-    pass
+    todos = Todo.objects.all().order_by('-id')
+
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        if text:
+            todo = Todo()
+            todo.text = text
+            todo.save()
+        else:
+            return render(request, 'home.html', {'error': "This fields is required!", 'todos': todos})
+
+    return render(request, 'home.html', {'todos': todos})
+
+def edit(request, id):
+    if request.method == 'GET':
+        try:
+            todo = Todo.objects.get(id=id)
+            return render(request, 'home.html', {'todo': todo})
+        except Todo.DoesNotExist:
+            return render(request, 'home.html', {'error': "Something went wrong!"})
+
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        if text:
+            try:
+                todo = Todo.objects.get(id=id)
+            except Todo.DoesNotExist:
+                return render(request, 'home.html', {'error': "Something went wrong!"})
+                
+            todo.text = text
+            todo.save()
+            return redirect('home')
+        else:
+            return render(request, 'home.html', {'error': "This fields is required!"})
+
+def delete(request, id):
+    if request.method == 'GET':
+        try:
+            todo = Todo.objects.get(id=id)
+        except Todo.DoesNotExist:
+            return render(request, 'home.html', {'error': "Something went wrong!"})
+        todo.delete()
+        return redirect('home')
+
+
+
