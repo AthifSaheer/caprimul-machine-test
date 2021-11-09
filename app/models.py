735416f1fbe32d0c6b14d6338f84567e8167e7c9
Athif Saheer <liteboook@gmail.com>
2021-11-09 12:30:19 +0530

    models and urls completed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    text = models.TextField(max_length=100)
+    done = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.text)
